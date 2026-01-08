5ee0ecd165f12eb2513ea20d715c6cf6e709cc3e
loowpts <pavelbariik@yandex.ru>
2026-01-08 15:44:06 +0300

    Настроена конфигурация проекта и подключены API endpoints
    Добавлены зависимости: Django REST Framework, JWT аутентификация, фильтры.
    Зарегистрированы все приложения системы в INSTALLED_APPS.
    Подключены API роуты для справочников, продуктов и активов.
    Миграции добавлены в .gitignore для упрощения работы с репозиторием.

--- settings/settings.py
+++ settings/settings.py
@@ -27,6 +27,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'django_filters',
+
+    'apps.references',
+    'apps.products',
+    'apps.assets',
+    'apps.stock',
+
 ]
 
 MIDDLEWARE = [
--- settings/urls.py
+++ settings/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
+    
+    path('api/v1/', include('apps.references.urls', namespace='references')),
+    path('api/v1/', include('apps.products.urls', namespace='products')),
+    path('api/v1/', include('apps.assets.urls', namespace='assets')),
+    
 ]
