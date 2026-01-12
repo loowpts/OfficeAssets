c485535f454b22700bffa4812e93d32d2f1d6e7d
loowpts <pavelbariik@yandex.ru>
2026-01-13 01:55:06 +0300

    Настроена админ-панель для управления списаниями
    - Зарегистрирована модель WriteOff в Django Admin
    - list_display: id, get_item, quantity, location, date, reason_short
    - list_filter по date и location
    - search_fields по reason, product__name, inventory_item__inventory_number
    - Метод get_item() - отображение названия списанного объекта
    - Метод reason_short() - отображение первых 50 символов причины
    - readonly_fields для date, created_at, updated_at

--- apps/writeoffs/admin.py
+++ apps/writeoffs/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import WriteOff
 
-# Register your models here.
+@admin.register(WriteOff)
+class WriteoffAdmin(admin.ModelAdmin):
+    list_display = [
+        'id', 'get_item', 'quantity',
+        'location', 'date', 'reason_short'
+    ]
+    list_filter = ['date', 'location']
+    search_fields = [
+        'reason', 'product__name',
+        'inventory_item__inventory_number'
+    ]
+    readonly_fields = ['date', 'created_at', 'updated_at']
+
+    def get_item(self, obj):
+        """Возвращает название списанного объекта"""
+        if obj.product:
+            return f'{obj.product.name} x{obj.quantity}'
+        return f'{obj.inventory_item.inventory_number}'
+    get_item.short_description = 'Списанный объект'
+
+    def reason_short(self, obj):
+        """Возвращает первые 50 символов причины"""
+        if len(obj.reason) > 50:
+            return f'{obj.reason[:50]}...'
+        return obj.reason
+    reason_short.short_description = 'Причина'
